e3e176f223e4bbf487014a6c70f971da3bfe2343
Alan Carpilovsky <8486092+alcar@users.noreply.github.com>
2019-03-01 15:11:29 -0300

    Add title to pages

--- react/Home.tsx
+++ react/Home.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Helmet } from 'vtex.render-runtime'
 
 import MarkdownBlock from './components/MarkdownBlock'
 
-const Home: React.SFC = () => <MarkdownBlock source="home/main" />
+const Home: React.SFC = () => (
+  <Fragment>
+    <Helmet>
+      <title>Render Guide</title>
+    </Helmet>
+    <MarkdownBlock source="home/main" />
+  </Fragment>
+)
 
 export default Home
--- react/Topic.tsx
+++ react/Topic.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Helmet } from 'vtex.render-runtime'
 
 import ErrorPage from './components/ErrorPage'
 import topics from './components/topics'
@@ -12,7 +13,16 @@ interface Props {
 const Topic: React.SFC<Props> = ({ params }) => {
   const topic = topics.find(entry => entry.slug === params.topic)
 
-  return topic ? <topic.Component /> : <ErrorPage />
+  return topic ? (
+    <Fragment>
+      <Helmet>
+        <title>{topic.name}</title>
+      </Helmet>
+      <topic.Component />
+    </Fragment>
+  ) : (
+    <ErrorPage />
+  )
 }
 
 export default Topic
