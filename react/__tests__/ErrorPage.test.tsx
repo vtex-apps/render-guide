c46826ea0f867591c55688399ca524ed2794565f
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-03-19 18:26:07 -0300

    Add `@vtex/test-tools`
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

---
+++ react/__tests__/ErrorPage.test.tsx
@@ -0,0 +1,12 @@
+import { render } from '@vtex/test-tools/react'
+import React from 'react'
+
+import ErrorPage from '../components/ErrorPage'
+
+test('should render ErrorPage component', () => {
+  const { getByText } = render(<ErrorPage />)
+
+  const element = getByText(/404/)
+
+  expect(element).toBeDefined()
+})
